f0984745298a685349e47123d6b54683fdee3d78
HarbySmart <155396836+HarbySmart@users.noreply.github.com>
2024-05-30 02:32:53 +0100

    Add files via upload

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,20 +1,20 @@
 import json
 import requests
 
-def emotion_detector(text_to_analyze):
-    url = ('https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict')
+def emotion_detector(text_to_analyse):
+    if not text_to_analyse.strip():  # Check for empty or blank input
+        return None
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    inputJson = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json = inputJson, headers = header)
+    myobj = {"raw_document": {"text": text_to_analyse}}
+    response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
 
-    #declare a variable emotion_predictions to call in order to extract the required emotions
-    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
-    emotion_predictor = {key: emotion_predictions[key] for key in emotion_predictions.keys()}
-    #below is the code for dominant_emotion
-    dominant_emotion = max(emotion_predictor, key = emotion_predictor.get)
-    
-    print(emotion_predictor)
-    
-    print(f"The dominant emotion is {dominant_emotion}")
-    
+    if response.status_code == 200:
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion.items(), key=lambda x: x[1])
+        return dominant_emotion[0]
+    else:
+        return None
+        
